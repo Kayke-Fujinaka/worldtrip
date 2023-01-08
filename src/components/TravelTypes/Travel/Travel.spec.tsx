e3a7027891500f3d57ad61d9b83ad568b19c2cb6
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-08 19:35:00 -0300

    test: Travel component

---
+++ src/components/TravelTypes/Travel/Travel.spec.tsx
@@ -0,0 +1,33 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { render, screen } from "@testing-library/react";
+import { Travel } from ".";
+import Surf from "../../../public/assets/surf.svg";
+
+describe("Travel component", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: query !== "(min-width: 700px)",
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+
+  it("should render responsive component", () => {
+    render(
+      <ChakraProvider>
+        <Travel image={Surf.src} text="praia" />
+      </ChakraProvider>
+    );
+
+    expect(screen.getByText("•")).toBeInTheDocument();
+    expect(screen.getByText("praia")).toBeInTheDocument();
+  });
+});
